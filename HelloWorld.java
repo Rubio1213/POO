252b459e5a87451a1b4d9bfc4b9e2b952c7b8664
Rubio1213 <102886756+Rubio1213@users.noreply.github.com>
2023-08-27 15:49:04 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,28 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World gg!"); 
+import java.util.*;
+
+public class Practica01 {
+    public static void main (String []args){
+        int a,b;
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Programa para conversion de bases numericas");
+        System.out.println("Ingresa la cantidad que deseas convertir");
+        a=Integer.parseInt(scanner.nextLine());//Pasar de string a entero
+        System.out.println("1.-Decimal a binario\n2.-Decimal a Octadecimal\n3.-Decimal a Hexadecimal");
+        System.out.println("Ingresa la opcion a convertir");
+        b=Integer.parseInt(scanner.nextLine());
+        switch (b){
+            case 1:
+                System.out.println("|| Decimal a binario ||");
+            break; 
+            case 2:
+                System.out.println("|| Decimal a Octadecimal ||");
+            break; 
+            case 3:
+                System.out.println("|| Decimal a Hexadecimal ||");
+            break; 
+            
+
+        }
+        
     }
 }
